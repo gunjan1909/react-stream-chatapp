8278329097c9b5ce59268ff2eca99eca41656d1f
gunjan1909 <bhanarkaranita@gmail.com>
2023-03-26 17:34:28 +0530

    .

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { router } from "./router";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,11 +1,14 @@
-import {createBrowserRouter} from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom";
+import { AuthLayout } from "./pages/layouts/AuthLayout";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
 
 export const router = createBrowserRouter([
-   {
-      element: <AuthLayout/>,
-      children: [
-         {path: "login", element: <Login/>},
-         {path: "signup", element: <:Signup/>},
-      ]
-   }
-])
+  {
+    element: <AuthLayout />,
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <Signup /> },
+    ],
+  },
+]);
