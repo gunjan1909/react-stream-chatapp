e9f19261d569edd1b387598c191aaf444c7e8d33
gunjan1909 <bhanarkaranita@gmail.com>
2023-03-29 23:54:32 +0530

    .

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,30 @@
+import {
+  LoadingIndicator,
+  Chat,
+  ChannelList,
+  Channel,
+  Window,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+} from "stream-chat-react";
+import { useAuth } from "../context/AuthContext";
+
 export function Home() {
+  const { user, streamChat } = useAuth();
+
+  if (streamChat == null) return <LoadingIndicator />;
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Chat client={streamChat}>
+      <ChannelList />
+      <Channel>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+      </Channel>
+    </Chat>
   );
 }
